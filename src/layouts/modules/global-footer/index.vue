1f453e3bcd4d94ca72c0ab84551b928e0fb3c5e4
Alaye-Dong <AlayeVast@outlook.com>
2026-01-02 15:50:11 +0800

    chore(footer): 更新版权信息和许可证链接

--- src/layouts/modules/global-footer/index.vue
+++ src/layouts/modules/global-footer/index.vue
@@ -6,8 +6,8 @@ defineOptions({
 
 <template>
   <DarkModeContainer class="h-full flex-center">
-    <a href="https://github.com/soybeanjs/soybean-admin/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
-      Copyright MIT © 2021 Soybean
+    <a href="https://github.com/Alaye-Dong/aigri-frontend/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
+      Copyright Apache 2.0 © 2026 AlayeDong
     </a>
   </DarkModeContainer>
 </template>
