4c2b6467b1b2471be6e726ddc999bae65274e1d4
Alaye-Dong <AlayeVast@outlook.com>
2026-03-04 20:25:18 +0800

    fix(eslint): 将eslint配置中的rules移动到overrides中

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,27 @@
 import { defineConfig } from '@soybeanjs/eslint-config';
 
-export default defineConfig(
-  { vue: true, unocss: true },
-  {
-    rules: {
-      'vue/multi-word-component-names': [
-        'warn',
-        {
-          ignores: ['index', 'App', 'Register', '[id]', '[url]']
-        }
-      ],
-      'vue/component-name-in-template-casing': [
-        'warn',
-        'PascalCase',
-        {
-          registeredComponentsOnly: false,
-          ignores: ['/^icon-/']
-        }
-      ],
-      'unocss/order-attributify': 'off',
-      'no-unused-vars': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'no-unused-expressions': 'warn',
-      'no-warning-comments': 'off'
-    }
+export default defineConfig({
+  vue: true,
+  unocss: true,
+  overrides: {
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index', 'App', 'Register', '[id]', '[url]']
+      }
+    ],
+    'vue/component-name-in-template-casing': [
+      'warn',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+        ignores: ['/^icon-/']
+      }
+    ],
+    'unocss/order-attributify': 'off',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-unused-expressions': 'warn',
+    'no-warning-comments': 'off'
   }
-);
+});
